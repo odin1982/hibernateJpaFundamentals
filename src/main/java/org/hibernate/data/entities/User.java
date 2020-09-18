1a5f73159fc2886a8f52c671386167c23d6d80df
odinPcWin10 <odin.araujo.barragan@gmail.com>
2020-09-17 20:05:39 -0500

    Se agrega un POJO de la tabla finances_user

---
+++ src/main/java/org/hibernate/data/entities/User.java
@@ -0,0 +1,73 @@
+package org.hibernate.data.entities;
+
+import java.time.LocalDateTime;
+/*
+ * Esto seria un simple POJO de la tabla finances_user
+ * */
+public class User {
+	private Long userId;
+	private String firstName;
+	private String lastName;
+	private LocalDateTime birthDate;
+	private String emailAddress;
+	private LocalDateTime lastUpdatedDate;
+	private String lastUpdatedBy;
+	private LocalDateTime createdDate;
+	private String createdBy;
+	
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public LocalDateTime getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(LocalDateTime birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public LocalDateTime getLastUpdatedDate() {
+		return lastUpdatedDate;
+	}
+	public void setLastUpdatedDate(LocalDateTime lastUpdatedDate) {
+		this.lastUpdatedDate = lastUpdatedDate;
+	}
+	public String getLastUpdatedBy() {
+		return lastUpdatedBy;
+	}
+	public void setLastUpdatedBy(String lastUpdatedBy) {
+		this.lastUpdatedBy = lastUpdatedBy;
+	}
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	
+}

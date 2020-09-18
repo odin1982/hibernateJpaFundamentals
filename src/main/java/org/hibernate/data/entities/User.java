6373eb31a64821149fd01a863ec53aff3733c4b4
odinPcWin10 <odin.araujo.barragan@gmail.com>
2020-09-17 20:19:02 -0500

    Simple Mapping

--- src/main/java/org/hibernate/data/entities/User.java
+++ src/main/java/org/hibernate/data/entities/User.java
@@ -1,18 +1,54 @@
 package org.hibernate.data.entities;
 
 import java.time.LocalDateTime;
-/*
- * Esto seria un simple POJO de la tabla finances_user
- * */
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 
+ * @author odin
+ * @Entity: indica que esta clase va estar mapeada a la tabña finances_user
+ * @Table(name="[nombre-tabla]") indica con que tabla va tener relacion
+ * @Id: Indica que este campo sera la clave primaria
+ * @GeneratedValue: Indica la forma en que se genera el autoincremento para tu llave primaria
+ * @Column: Indica a que columna de la tabla hace referencia
+ */
+@Entity 
+@Table(name="FINANCES_USER")
 public class User {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="USER_ID")
 	private Long userId;
+	
+	@Column(name="FIRST_NAME")
 	private String firstName;
+	
+	@Column(name="LAST_NAME")
 	private String lastName;
+	
+	@Column(name="BIRTH_DATE")
 	private LocalDateTime birthDate;
+	
+	@Column(name="EMAIL_ADDRESS")
 	private String emailAddress;
+	
+	@Column(name="LAST_UPDATED_DATE")
 	private LocalDateTime lastUpdatedDate;
+	
+	@Column(name="LAST_UPDATED_BY")
 	private String lastUpdatedBy;
+	
+	@Column(name="CREATED_DATE")
 	private LocalDateTime createdDate;
+	
+	@Column(name="CREATED_BY")
 	private String createdBy;
 	
 	public Long getUserId() {
